d26b2a04f09317653278d9a54331d89aaef6bf32
Паймышев Никита <nikita.paymyshev@gmail.com>
2026-09-18 00:19:54 +0700

    feat: Улучшен BankAccount

--- src/account.py
+++ src/account.py
@@ -6,11 +6,44 @@ class BankAccount:
         self.account_number = account_number
         self.balance = balance
 
-        self.transactions_history = list[dict]
+        self.transactions_history: list[dict] = []
 
 
+    def __str__(self) -> str:
+        return f"Номер счета: {self.account_number}| Баланс: {self.balance:.2f}"
 
-    def deposit(self, amount: float) -> None:
-        if 0 > amount:
-            raise ValueError("Сумма пополнения должна быть больше 0.")
+
+    def deposit(self, amount: float) -> bool:
+        if 0 >= amount:
+           raise ValueError("Сумма пополнения должна быть больше 0.")
+
+        self.balance += amount
+
+        transaction: dict = {
+            "type": "deposit",
+            "amount": amount,
+            "datetime": datetime.now()
+        }
+
+        self.transactions_history.append(transaction)
+
+        return True
+
+    def get_balance(self) -> float:
+        return self.balance
+
+    def withdraw(self, amount: float) -> bool:
+        if amount < self.balance:
+            raise ValueError("Суммы на балансе не хватает что-бы совершить операцию.")
+
+        self.balance -= amount
+        transaction: dict = {
+            "type": "withdraw",
+            "amount": -amount,
+            "datetime": datetime.now()
+        }
+
+        self.transactions_history.append(transaction)
+        return True
+
 
--- src/customer.py
+++ src/customer.py
@@ -1,3 +1,12 @@
 import account
 
-my_account = account.BankAccount("123", )
+my_account = account.BankAccount("123", 100)
+
+print(my_account)
+
+try:
+    my_account.deposit(0)
+except ValueError as e:
+    print(e)
+
+print(f"Баланс: {my_account.get_balance()}")
